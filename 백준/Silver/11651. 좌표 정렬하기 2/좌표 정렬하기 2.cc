4c1fd1f4177428d2e4927fd11a8dfc8ccd41c33f
Seiyeon Joo <94334477+seiyeon@users.noreply.github.com>
2023-06-28 15:23:07 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 136 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<pair<int, int>>v;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back({y,x});
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<< v[i].second<<' '<<v[i].first<<'\n';
+    }
+    
+    return 0;
+}
