243b4d2cb57552d7ebc6e8d99bc8c32cc0e2892d
Seiyeon Joo <94334477+seiyeon@users.noreply.github.com>
2023-07-29 16:44:00 +0900

    [Silver V] Title: K번째 수, Time: 1108 ms, Memory: 21432 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<< arr[k-1];
+    
+    return 0;
+}
