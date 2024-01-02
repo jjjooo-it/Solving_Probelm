ed5e8ce513b34e410fbab99dbcb8a42ac09f26ad
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2024-01-02 19:26:14 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    int result=0;
+    cin >> s;
+
+    
+    for(int i=0;i<s.length();i++){
+        if(s[i]!=s[i+1])result++;
+    }
+        
+    if(!result)cout << result;
+    else cout << result/2;
+    return 0;
+}
